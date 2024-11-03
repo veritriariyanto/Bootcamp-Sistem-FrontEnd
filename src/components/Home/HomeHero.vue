64b7727603555b24a5886f97b67ef36d279361ce
VeriAriyanto <veritriariyanto@gmail.com>
2024-11-03 09:16:28 +0700

    update

--- src/components/Home/HomeHero.vue
+++ src/components/Home/HomeHero.vue
@@ -1,36 +1,37 @@
 <template>
-  <div
-    class="hero h-[720px] w-auto font-xinter"
-    style="
-      background-image: url(https://s3-alpha-sig.figma.com/img/6da6/c3e5/0ef0427f95f95d6c279b57dcfb8178ef?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CUt08KwwNBPaaIGxe-s~~42rCw30G6JFYzwbw5T6mMiqfS4I~DCKR2Zc4DG~ERptoSCTWUSOnqjI~mDcwWRglpV1DoCh0l~MReXlq3Pa84~LZACMAM6OZDAKE~7eZSSrnzMabWEetECIx1JFm7s0qfinWOn2tlY8DvjMrmgRde0RB5QeCWY4In3NfZrGzYCECKzP1CHHx85I~BVluPUPANy9be7L6Tq3hLusnZm8vwXjb5EmkPAG-ch9P0l3t0uz6qKcDYqdCAVVD3WYCmnX~qADa5Srr0rBfGEhGdq3ZtDCwhbIEJ4Q7A37wPj9lebhiNP8m-VgfW2MEq2N9e9LcQ__);
-      transform: scaleX(-1);
-    "
-  >
+  <div class="hero min-h-screen relative overflow-hidden text-x-light">
+    <img
+      src="\src\assets\images\Home\bg1.png"
+      alt="Hero Background"
+      class="absolute inset-0 w-full h-full object-cover transform scale-x-[1] z-0"
+    />
+
     <div
-      class="hero-overlay flex flex-col lg:flex-row-reverse pt-16 lg:pl-20 items-center"
+      class="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_rgba(0,0,0,0)_5%,_rgba(0,0,0,0.8))] z-5"
+    ></div>
+    <div
+      class="hero flex flex-col lg:flex-row-reverse lg:justify-end relative z-10 bg-black bg-opacity-60 text-white p-10 lg:p-20"
     >
-      <div class="mr-10 pb-20 text-x-light w-full lg:w-9/12 scale-x-[-1]">
-        <h5 class="text-base uppercase">
+      <div class="lg:text-left">
+        <h5 class="text-sm uppercase mb-2">
           Posted on <span class="font-bold">startup</span>
         </h5>
-        <h1
-          class="font-sans text-4xl md:text-5xl lg:text-6xl font-bold leading-tight"
-        >
+        <h1 class="text-4xl lg:text-6xl font-bold leading-tight">
           Step-by-step guide to choosing <br />
           great font pairs
         </h1>
-        <h5 class="text-base mb-5">
-          By <span class="text-x-blue font-xinter">James West</span> | May 23,
-          2022
+        <h5 class="text-sm my-4">
+          By <span class="text-x-blue">James West</span> | May 23, 2022
         </h5>
-        <p class="text-base mb-10">
+        <p class="mb-6">
           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
           dolore eu <br />
           fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
         </p>
         <a
-          class="py-4 px-12 btn border-none bg-x-blue text-x-light rounded-none"
-          >Read More ></a
+          href="#"
+          class="btn bg-x-blue rounded-none border-none px-12 py-4 text-x-light"
+          >Read More &gt;</a
         >
       </div>
     </div>
